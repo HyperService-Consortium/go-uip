62e046f0c5775baf454e488ab2ea6b525ea7e6e2
Myriad-Dreamin <camiyoru@gmail.com>
2019-08-08 14:16:08 +0800

    Create merkleproof.go

---
+++ types/merkleproof.go
@@ -0,0 +1,22 @@
+package types
+
+type merkleProofType = uint16
+type proof = []byte
+type key = []byte
+type value = []byte
+
+// MerkleProof is the merkle proof that proves the existence of key-value pair on the  merkle tree
+type MerkleProof interface {
+	GetType() uint16
+	GetProof() []byte
+	GetKey() []byte
+	GetValue() []byte
+}
+
+// BlockCheck is the merkle proof that proves the existence of merkle tree (root hash) on the  block
+type BlockCheck interface {
+	GetChainId() uint64
+	GetBlockId() []byte
+	GetRootTag() uint8
+	GetRootHash() []byte
+}
